dc5da970bf1281efcf6f7c602e9efd661a8c21e9
Nic Rosental <nic@epiclabs.com>
2021-10-16 04:54:14 -0400

    button changes if post already has a project

--- templates/metaboxes/edit.php
+++ templates/metaboxes/edit.php
@@ -1,6 +1,12 @@
 <div>
-    <form>
+
+    <?php
+    if ($plUrl = get_post_meta(get_the_id(), 'pl-project-url', true)) {
+
+    ?>
+        <a class="button button-secondary" href="<?php echo $plUrl ?>" target="_blank">Go To Punchlist Project</a>
+    <?php } else { ?>
         <?php wp_nonce_field('pl-create-project-edit-screen', 'plnonce'); ?>
         <a class="button button-primary" id="pl-create-project-edit-screen">Create a Punchlist Project</a>
-    </form>
+    <?php } ?>
 </div>
